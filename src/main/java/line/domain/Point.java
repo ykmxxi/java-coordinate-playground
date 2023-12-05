aefdf3e48e7599d0e7538c945bbd175b6249b0cb
ykmxxi <ykmxxi97@gmail.com>
2023-12-05 11:31:57 +0900

    feat(Point): 점의 좌표 범위 예외 처리 기능 구현

--- src/main/java/line/domain/Point.java
+++ src/main/java/line/domain/Point.java
@@ -4,12 +4,26 @@ import java.util.Objects;
 
 public class Point {
 
+    private static final int MIN_COORDINATE = 0;
+    private static final int MAX_COORDINATE = 24;
+
     private final int x;
     private final int y;
 
     public Point(final int x, final int y) {
+        validate(x, y);
         this.x = x;
         this.y = y;
+    }
+
+    private void validate(final int x, final int y) {
+        if (!isValidRange(x) || !isValidRange(y)) {
+            throw new IllegalArgumentException("[ERROR] 좌표의 범위는 0 ~ 24 입니다.");
+        }
+    }
+
+    private boolean isValidRange(final int coordinate) {
+        return coordinate >= MIN_COORDINATE && coordinate <= MAX_COORDINATE;
     }
 
     @Override
--- src/test/java/line/domain/PointTest.java
+++ src/test/java/line/domain/PointTest.java
@@ -1,9 +1,13 @@
 package line.domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 class PointTest {
 
@@ -15,6 +19,25 @@ class PointTest {
 
         // when & then
         assertThat(point).isEqualTo(new Point(10, 10));
+    }
+
+    @DisplayName("좌표 범위 0 ~ 24를 벗어난 경우 IllegalArgumentException 예외가 발생한다.")
+    @ParameterizedTest(name = "x좌표: {0}, y좌표: {1}")
+    @CsvSource(value = {"-1:0", "25:0", "0:-1", "0:25"}, delimiter = ':')
+    void 좌표_범위_예외_처리(int x, int y) {
+        // when & then
+        assertThatThrownBy(() -> new Point(x, y))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 좌표의 범위는 0 ~ 24 입니다.");
+    }
+
+    @DisplayName("좌표 범위를 만족한 경우 점이 생성된다.")
+    @ParameterizedTest(name = "x좌표: {0}, y좌표: {1}")
+    @CsvSource(value = {"0:0", "24:24"}, delimiter = ':')
+    void 좌표_범위_예외_처리_통과(int x, int y) {
+        // when & then
+        assertThatCode(() -> new Point(x, y))
+                .doesNotThrowAnyException();
     }
 
 }
